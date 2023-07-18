251b202a1721ab61a05b4d2c641f2dc36983cc56
Marsolo1 <martialmarzloff@gmail.com>
2023-07-18 17:32:34 +0900

    added UI_test

---
+++ UI_test.py
@@ -0,0 +1,41 @@
+### create a window using tkinter, that can take as input a picture from the computer and display it on the screen
+### the picture is resized to fit the window
+
+from tkinter import *
+from PIL import ImageTk, Image
+from tkinter import filedialog
+
+from street_clip import predict, hiyoshi_maps
+
+root = Tk()
+root.title('Image Viewer')
+
+# resize window to 500x300px
+root.geometry("500x300")
+
+
+# open image
+def open_img():
+	global my_img
+	root.filename = filedialog.askopenfilename(initialdir="~", title="Select your picture", filetypes=(("image file", ("*.png", "*.jpg", "*.jpeg")), ("all files", "*.*")))
+	my_label = Label(root, text=root.filename).pack()
+	my_img = ImageTk.PhotoImage(Image.open(root.filename))
+	my_img_label = Label(image=my_img).pack()
+	return my_img
+
+
+# display the result of prediction
+def display_result():
+	res = predict(Image.open(root.filename), list(hiyoshi_maps.keys()))
+	result_label = Label(root, text=res).pack()
+	return res
+
+
+# create an open image button
+my_btn = Button(root, text="Open Image", command=open_img).pack(anchor=CENTER)
+
+# create a predict button
+predict_btn = Button(root, text="Predict", command=display_result).pack(anchor=CENTER)
+
+if __name__ == '__main__':
+	root.mainloop()
--- street_clip.py
+++ street_clip.py
@@ -22,3 +22,13 @@ def predict(img: np.ndarray, labels: List[str]):
     probs: torch.Tensor = logits_per_image.softmax(dim=1)
     index = torch.argmax(probs, dim=1).item()
     return labels[index]
+
+hiyoshi_maps = {
+    "1-chome": "〒223-0061 神奈川県横浜市港北区日吉1丁目",
+    "2-chome": "〒223-0061 神奈川県横浜市港北区日吉2丁目",
+    "3-chome": "〒223-0061 神奈川県横浜市港北区日吉3丁目",
+    "4-chome": "〒223-0061 神奈川県横浜市港北区日吉4丁目",
+    "5-chome": "〒223-0061 神奈川県横浜市港北区日吉5丁目",
+    "6-chome": "〒223-0061 神奈川県横浜市港北区日吉6丁目",
+    "7-chome": "〒223-0061 神奈川県横浜市港北区日吉7丁目",
+}
